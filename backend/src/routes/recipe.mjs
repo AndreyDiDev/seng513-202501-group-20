9e94003781212351cf1ce58e5ed2967e463d90bf
JuanPCF1 <107865734+JuanPCF1@users.noreply.github.com>
2025-04-16 00:29:19 -0600

    Comment out some lines for merge

--- backend/src/routes/recipe.mjs
+++ backend/src/routes/recipe.mjs
@@ -2,10 +2,10 @@ import {Router} from "express";
 const router = Router();
 import {createRecipeController} from '../controllers/recipeController.js'
 import {removeRecipeController} from '../controllers/recipeController.js';
-import {getRecipesByIngredients} from '../controllers/recipeController.js';
+//import {getRecipesByIngredients} from '../controllers/recipeController.js';
 
 router.post('/api/recipe', createRecipeController);
 router.delete('/api/recipe/:id', removeRecipeController);
-router.get('/api/recipe', getRecipesByIngredients);
+//router.get('/api/recipe', getRecipesByIngredients);
 
 export default router
