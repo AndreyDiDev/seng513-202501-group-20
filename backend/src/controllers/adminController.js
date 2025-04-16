61c4d273f684f2b76af40d4d6957910796b95212
Zaid Nissar <zupanditzaid@gmail.com>
2025-04-16 17:11:51 -0600

    Admin can now see all users and comments and delete them. Admin can also make a user normal or premium

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -47,7 +47,6 @@ export const adminDeleteCommentController = async (req,res)=>{
     try{
         const {commentID} = req.body;
         const comment = await db.Comment.findByPk(commentID)
-        console.log('COMMMENNTTTT ' + comment)
         await comment.destroy()
         res.status(200).json({messgae : 'Deleted comment'})
     }catch(err){
@@ -55,6 +54,67 @@ export const adminDeleteCommentController = async (req,res)=>{
         res.status(500).json({error: "Failed to delete comment: "+err})
     }
 }
+
+export const adminGetAllUsersController = async (req ,res)=>{
+    try {
+        const allUsers = await db.User.findAll()
+        res.status(200).json(allUsers)
+    } catch (err) {
+        console.log(err)
+        res.status(404).json({error: "Failed to get requested resources: " + err})
+    }
+}
+
+export const adminDeleteUserController = async (req,res)=>{
+    try{
+        const {userID} = req.body;
+        const user = await db.User.findByPk(userID)
+        await user.destroy()
+        res.status(200).json({messgae : 'Deleted user'})
+    }catch(err){
+        console.log(err)
+        res.status(500).json({error: "Failed to delete user: "+err})
+    }
+}
+
+export const adminMakeUserPremiumController = async (req, res) => {
+    try {
+        const { userID} = req.body;
+
+        const user = await db.User.findByPk(userID);
+        if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+        }
+
+        user.role = 'premium';
+        await user.save();
+
+        res.status(200).json({ message: `User ${user.name} is now a premium member.` });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to update user role' });
+    }
+};
+  
+export const adminMakeUserNormalController = async (req, res) => {
+    try {
+        const { userID } = req.body;
+
+        const user = await db.User.findByPk(userID);
+        if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+        }
+
+        user.role = 'normal';
+        await user.save();
+
+        res.status(200).json({ message: `User ${user.name} is now a normal user.` });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to update user role' });
+    }
+};
+
 
 // export const addComment = async (req,res)=>{
 //     try {
--- backend/src/routes/admin.mjs
+++ backend/src/routes/admin.mjs
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { adminDeleteCommentController, createAdminController, getAdminCommentsController, getAdminDashboardController } from "../controllers/adminController.js";
+import {
+    adminDeleteCommentController, adminDeleteUserController, adminGetAllUsersController, 
+    adminMakeUserNormalController, adminMakeUserPremiumController, createAdminController, 
+    getAdminCommentsController, getAdminDashboardController } from "../controllers/adminController.js";
 import { isAuthenticated } from "../utils/isAuthenticated.mjs";
 import { requireRole } from "../utils/requireRole.mjs";
 
@@ -12,4 +15,15 @@ router.get('/api/admin/comments', isAuthenticated, requireRole('admin'), getAdmi
 // requires id as commentID in json body and deletes that comment
 router.post('/api/admin/comments', isAuthenticated, requireRole('admin'), adminDeleteCommentController)
 
+
+//user routes for admin to manage users 
+router.get('/api/admin/users', isAuthenticated, requireRole('admin'), adminGetAllUsersController)
+//requires id as userID in json body to delete the user 
+router.post('/api/admin/users', isAuthenticated, requireRole('admin'), adminDeleteUserController)
+
+
+//premium and normal role change routes 
+//routes expect userID in json body
+router.post('/api/admin/premium', isAuthenticated, requireRole('admin'), adminMakeUserPremiumController)
+router.post('/api/admin/normal', isAuthenticated, requireRole('admin'), adminMakeUserNormalController)
 export default router;
