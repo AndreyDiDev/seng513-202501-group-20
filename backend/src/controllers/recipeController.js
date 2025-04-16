03d3e74e55baa5245f38d1c81a79bcfaacb8e3cc
JuanPCF1 <107865734+JuanPCF1@users.noreply.github.com>
2025-04-15 21:05:53 -0600

    Complete Recipe Controller [POST]

--- backend/src/controllers/recipeController.js
+++ backend/src/controllers/recipeController.js
@@ -3,13 +3,30 @@ import db from '../db/db.js';
 import { hashPassword } from '../utils/authHelper.mjs';
 
 export const createRecipeController = async (req, res) => {
+    const t = await db.sequelize.transaction(); // Begin transaction
     try {
-        const {id, title, time, calories, instructions, createdAt, updatedAt, createdBy} = req.body;
-        const recipe = await db.Recipe.create({id, title, time, calories, instructions, createdAt, updatedAt, createdBy});
+        const { title, time, calories, instructions, createdBy, ingredients } = req.body;
+        const recipe = await db.Recipe.create({ title, time, calories, instructions, createdBy }, { transaction: t });
+
+        // Process ingredients safely
+        const ingredientRecords = [];
+        for (const name of ingredients) {
+        const [ingredient] = await db.Ingredient.findOrCreate({
+            where: { name },
+            transaction: t
+        });
+        ingredientRecords.push(ingredient);
+        }
+
+        await recipe.addIngredients(ingredientRecords, { transaction: t });
+        await t.commit(); // Commit transaction
+
         console.log('Recipe created:', recipe);
         res.status(201).json(recipe);
     } catch (err) {
+        await t.rollback(); // Roll back if something fails
         console.error(err);
         res.status(500).json({ error: 'Failed to create recipe' });
     }
 };
+  
