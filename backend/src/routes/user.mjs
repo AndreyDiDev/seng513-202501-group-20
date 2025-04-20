dd5bb295b2b37ba20bc36dfc6a0ec0cd98203ee9
JuanPCF1 <107865734+JuanPCF1@users.noreply.github.com>
2025-04-20 12:42:25 -0600

    Fixed an Import again

--- backend/src/routes/user.mjs
+++ backend/src/routes/user.mjs
@@ -3,6 +3,7 @@ const router = Router();
 import {createUserController} from '../controllers/userController.js'
 import { getUserFromEmail } from "../controllers/userController.js";
 import { isAuthenticated } from "../utils/isAuthenticated.mjs";
+import { requireRole } from "../utils/requireRole.mjs";
 
 
 router.post('/api/user', createUserController);
